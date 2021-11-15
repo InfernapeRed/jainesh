f69f986d86152f2bd425f9a6aae91d1608f8f716
InfernapeRed <69681031+InfernapeRed@users.noreply.github.com>
2021-11-15 11:09:43 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import socket
-HEADER=64
+HEADER=1024
 FORMAT='utf-8'
 DISCONNECT_MESSAGE='!DISCONNECT'
 SERVER=input("Enter server IP:")
@@ -8,19 +8,19 @@ ADDR=(SERVER,PORT)
 
 client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect(ADDR)
-
+print(client.recv(HEADER).decode(FORMAT))
 def send(msg):
     message=msg.encode(FORMAT)
     client.send(message)
-    recv_msg=client.recv(HEADER).decode(FORMAT)
-    return recv_msg
+    return msg
 connected=True
+
 while connected:
-    recv_msg=send(input("> "))
-    if recv_msg:
-        if recv_msg==DISCONNECT_MESSAGE:
+    sent_msg=send(input("> "))
+    if sent_msg:
+        if sent_msg==DISCONNECT_MESSAGE:
             print("you are disconnecting...\n DISCONNECTED")
             connected=False
             break
-        print(recv_msg)
         
+
